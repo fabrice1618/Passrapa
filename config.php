a0a57c1a63947c17961c6ac0c0e6eeb7b3ef63e7
SoCag <so.cagnolati@gmail.com>
2022-06-22 09:20:31 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+// Pepper hash
+$pepper = 'c1isvFdxMDdmjOlvxpecFw';
+
+// On se connecte à la base de données
+$user = 'passrapa';
+$pass = 'toto';
+
+try
+{
+    // On se connecte à MySQL
+	$dbh = new PDO('mysql:host=localhost;dbname=passrapa', $user, $pass);
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        exit('Erreur : '.$e->getMessage());
+}
+
+?>
